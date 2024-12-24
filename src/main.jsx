f40cab8abf236041f90cfe26a19f9e722517ae52
yogaekapratama29 <yogaex29@gmail.com>
2024-12-25 00:51:28 +0700

    +add preventDefault

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,7 @@ import Table from './table/Table.jsx'
 import AlertButton from './button/AlertButton.jsx'
 import MyButton from './button/MyButton.jsx'
 import Toolbar from './button/Toolbar.jsx'
+import SearchForm from './form/SearchForm.jsx'
 
 
 createRoot(document.getElementById('root')).render(
@@ -27,6 +28,8 @@ createRoot(document.getElementById('root')).render(
         e.stopPropagation();
         alert("You Click Toolbar");
       }}/>
+
+      <SearchForm/>
     </Container>
   </StrictMode>,
 )
