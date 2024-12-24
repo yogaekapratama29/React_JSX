1750137e387ab4bf69808328a2e19566b5b1290e
yogaekapratama29 <yogaex29@gmail.com>
2024-12-25 01:09:57 +0700

    +add UseState

--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,7 @@ import MyButton from './button/MyButton.jsx'
 import Toolbar from './button/Toolbar.jsx'
 import SearchForm from './form/SearchForm.jsx'
 import SayHelloForm from './form/SayHelloForm.jsx'
+import Counter from './form/Counter.jsx'
 
 
 createRoot(document.getElementById('root')).render(
@@ -32,6 +33,7 @@ createRoot(document.getElementById('root')).render(
 
       <SearchForm/>
       <SayHelloForm/>
+      <Counter/>
     </Container>
   </StrictMode>,
 )
