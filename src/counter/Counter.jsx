565979a6f77af2ab8cdbe7835bbd70c953340ae4
yogaekapratama29 <yogaex29@gmail.com>
2024-12-25 23:30:15 +0700

    + add mempertahankan state

---
+++ src/counter/Counter.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react"
+
+export default function Counter({name}){
+    const [count,setCount] = useState(0);
+
+    function handleClick(){
+        setCount(count + 1);
+    }
+    return(
+       <div>
+        <h1>Counter {name} : {count}</h1>
+        <button onClick={handleClick}>Increment</button>
+       </div>
+    )
+}
---
+++ src/counter/CounterApp.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import Counter from "./Counter";
+
+export default function CounterApp(){
+    const [show2,setShow2] = useState(true);
+    
+    function handleChange(e){
+        setShow2(e.target.checked);
+    }
+
+    return (
+        <div>
+            
+            {show2 ? <Counter name="Eka"/> : <Counter name= "Yoga"/>}
+            <input type="checkbox" checked = {show2} onChange={handleChange} /> Tampilkan Counter 2
+        </div>
+    )
+}
