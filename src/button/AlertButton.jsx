37315b4d36501f782b12accb97f1ec462fcaad28
yogaekapratama29 <yogaex29@gmail.com>
2024-12-26 20:31:15 +0700

    + add useRef (Menyimpan Data tanpa render ulang)

--- src/button/AlertButton.jsx
+++ src/button/AlertButton.jsx
@@ -1,7 +1,10 @@
+import { useRef } from "react";
+
 export default function AlertButton({text,message}){
+    const counter = useRef(0);
     function handleClick(e){
         console.info(e);
-        alert(message);
+        alert(`${message} ${counter.current++}`);
     }
 
     return (
