5b9a3800f68c417c3bcc8ee7edeca5aea06a370f
yogaekapratama29 <yogaex29@gmail.com>
2024-12-26 21:53:27 +0700

    +add useEffect cleanup

--- src/product/ProductList.jsx
+++ src/product/ProductList.jsx
@@ -18,6 +18,9 @@ export default function ProductList() {
         .then(() => {
           loaded.current = true;
         });
+        return(()=>{
+            console.log("cleanup");
+        });
     }
   });
   return (
